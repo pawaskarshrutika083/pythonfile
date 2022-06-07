fffb2f34533684429ac3ef28e6d0309c8e092a22
pawaskarshrutika083 <pawaskarshrutika083@gmail.com>
2022-06-07 20:10:49 +0530

    no message

---
+++ first.py
@@ -0,0 +1,9 @@
+a=int(input("enetr number"))
+b=int(input("enetr number"))
+print(a+b)
+if a>b:
+    print("in if")
+    print("a is greater",a)
+else:
+    print("in else")
+    print("b is greater",b)
